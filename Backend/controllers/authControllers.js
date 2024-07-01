9653045c04c9f278254d9be0fb50af42f4397ec9
anh7012 <phamthingocanh30102@gmail.com>
2024-07-02 02:42:31 +0700

    create: auth

--- Backend/controllers/authControllers.js
+++ Backend/controllers/authControllers.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcrypt")
 const User = require("../models/Users")
 const jwt = require("jsonwebtoken")
 const resStatus = require("../resStatus");
+const Users = require("../models/Users");
 
 let refreshTokens = []
 const authControllers = {
@@ -23,50 +24,31 @@ const authControllers = {
             process.env.JWT_ACCESS_KEY_REFRESH,
             {expiresIn: "365d"})
     },
-    registerUser: async (req, res) => {
-        try {
-            const salt = await bcrypt.genSalt(10)
-            const hashed = await bcrypt.hash(req.body.password, salt)
-
-            // create user
-            const newUser = await new User({
-                username: req.body.username,
-                email: req.body.email,
-                password: hashed,
-                role: req.body.role
-            })
-
-            // save to  DB
-            const user = await newUser.save()
-            res.status(resStatus.OK).json(user)
-        } catch (err) {
-            res.status(resStatus.INTERNAL_SERVER_ERROR).json(err)
-        }
-    },
     loginUser: async (req, res) => {
         try {
-            const user = await User.findOne({username: req.body.username})
-            if(!user) {
-                return res.status(resStatus.NOT_FOUND).json("Wrong password")
+            const user = await Users.getUserByUsername(req.body.username)
+            if (!user) {
+                return res.status(resStatus.NOT_FOUND).json("user doesn't exist")
+            } else if (user.status === 'close') {
+                return res.status(resStatus.NOT_FOUND).json("user is closed")
             }
             const validPassword = await bcrypt.compare(
                 req.body.password,
                 user.password
             )
             if (!validPassword) {
-               return  res.status(resStatus.NOT_FOUND).json("Wrong password")
-            }
-            if (validPassword && user) {
+                return res.status(resStatus.NOT_FOUND).json("Wrong password")
+            } else if (validPassword && user) {
                 const accessToken = authControllers.generateAccessToken(user)
                 const refreshToken = authControllers.generateRefreshToken(user)
                 refreshTokens.push(refreshToken)
-                res.cookie("refreshToken",refreshToken , {
-                    httpOnly:true,
-                    path:"/",
-                    sameSite:"strict",
-                    secure:false
+                res.cookie("refreshToken", refreshToken, {
+                    httpOnly: true,
+                    path: "/",
+                    sameSite: "strict",
+                    secure: false
                 })
-                const {password, ...other} = user._doc
+                const {password, ...other} = user
                 res.status(resStatus.OK).json({user: other, accessToken: accessToken})
             }
         } catch (err) {
@@ -74,36 +56,39 @@ const authControllers = {
         }
     },
 
-    requestRefreshToken: async (req,res)=>{
+    requestRefreshToken: async (req, res) => {
         const refreshToken = req.cookies.refreshToken
         if (!refreshToken) return res.status(resStatus.UNAUTHORIZED).json("You're not authenticated")
-        if (!refreshTokens.includes(refreshToken)){
+        if (!refreshTokens.includes(refreshToken)) {
             return res.status(resStatus.FORBIDDEN).json("Refresh token is not valid")
         }
-        jwt.verify(refreshToken,process.env.JWT_ACCESS_KEY_REFRESH,(err,user)=>{
-            if(err){
+        jwt.verify(refreshToken, process.env.JWT_ACCESS_KEY_REFRESH, (err, user) => {
+            if (err) {
                 console.log(err)
-            }
-           else {
-                refreshTokens = refreshTokens.filter((token)=>token!== refreshToken)
+            } else {
+                refreshTokens = refreshTokens.filter((token) => token !== refreshToken)
                 // Create new accessToken amd refreshToken
                 const newAccessToken = authControllers.generateAccessToken(user)
                 const newRefreshToken = authControllers.generateRefreshToken(user)
                 refreshTokens.push(newRefreshToken)
-                res.cookie("refreshToken",newRefreshToken , {
-                    httpOnly:true,
-                    path:"/",
-                    sameSite:"strict",
-                    secure:false
+                res.cookie("refreshToken", newRefreshToken, {
+                    httpOnly: true,
+                    path: "/",
+                    sameSite: "strict",
+                    secure: false
                 })
-                res.status(resStatus.OK).json({accessToken:newAccessToken})
+                res.status(resStatus.OK).json({accessToken: newAccessToken})
             }
         })
     },
-    logoutUser: async(req,res)=>{
-        res.clearCookie("refreshToken")
-        refreshTokens = refreshTokens.filter(token => token !== req.cookies.refreshToken)
-        res.status(resStatus.OK).json("Logged out!")
+    logoutUser: async (req, res) => {
+        try {
+            res.clearCookie("refreshToken")
+            refreshTokens = refreshTokens.filter(token => token !== req.cookies.refreshToken)
+            res.status(resStatus.OK).json("Logged out!")
+        } catch (err) {
+            res.status(resStatus.INTERNAL_SERVER_ERROR).json(err)
+        }
     }
 }
 module.exports = authControllers
